3763ae7b64f636126fb14df105668f67f041c9de
Sujit Samanta <sujitsamanta510@gmail.com>
2025-11-02 12:50:45 +0530

    'password' => 'hashed', comment out

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,10 +81,10 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            
         ];
     }
-
+// 'password' => 'hashed',
 
     protected static function boot()
     {
